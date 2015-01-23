9864c9c069d0090ad61f67081e3f3b58957e5f1b
Jason P. Stanford <jps3@lehigh.edu>
2015-01-23 10:28:25 -0500

    Updated for PathFinder 7 (PF7)
    * Chanced description strings, comments, etc to remove explicit
      references to PathFinder 6, and not refer to a specific version
      number.
    * Added additional try/except for the XML parsing step to provide
      a more informative error message to differentiate from an
      HTTP error.
    * Updated example XML code in comments at bottom with what was
      retrieved from update URL while testing these changes.

--- PathFinder/PathFinderURLProvider.py
+++ PathFinder/PathFinderURLProvider.py
@@ -9,14 +9,14 @@ __all__ = ["PathFinderURLProvider"]
 
 class PathFinderURLProvider(Processor):
 
-    description = "Provides URL for the latest version of PathFinder 6"
+    description = "Provides URL for the latest version of PathFinder."
     input_variables = {}
     output_variables = {
         "version": {
             "description": "Version of the PathFinder download.",
         },
         "url": {
-            "description": "URL to the latest PathFinder 6 release download.",
+            "description": "URL to the latest PathFinder release download.",
         },
     }
     __doc__ = description
@@ -24,20 +24,24 @@ class PathFinderURLProvider(Processor):
 
 
     def main(self):
-        """Identify and return the download URL for the latest version of PathFinder 6"""
+        """Identify and return the download URL for the latest version of PathFinder"""
         update_url = "%s/%s" % (self.env['UPDATE_XML_HOST'], self.env['UPDATE_XML_PATH'])
         try:
             f = urllib2.urlopen(update_url) 
+        except BaseException as e:
+            raise ProcessorError("Unable to download %s: %s" % (update_url, e))
+
+        try:
             xml = f.read()
             doc = ET.fromstring(xml)
         except BaseException as e:
-            raise ProcessorError("Unable to download %s: %s" (update_url, e))
+            raise ProcessorError("Error attempting to parse XML data from update feed at: %s (Error: %s)" % (update_url, e) )
         else:
             self.env["url"] = doc.find('url').text
             self.output("Found URL for update download: %s" % self.env["url"])
             self.env["version"] = doc.find('version').text
             self.output("Found version for update download: %s" % self.env["version"])
-            
+
 
 
 if __name__ == '__main__':
@@ -48,14 +52,24 @@ if __name__ == '__main__':
 # Example returned data:
 # <?xml version="1.0" encoding="utf-8"?>
 # <item>
-#     <build>1561</build>
-#     <version>6.5.3</version>
+#     <build>1672</build>
+#     <version>7.1.1</version>
 #     <minOS>10.7</minOS>
-#     <url>http://get.cocoatech.com/PF6_LION.zip</url>
+#     <url>http://get.cocoatech.com/PF7.zip</url>
 #     <notes><![CDATA[
-#         <h3>Path Finder 6.5.3 is now available!</h3>
-#         <p>Changes:</p>
-#         <p><b>1.</b> Fixes to some instability issues introduced in 6.5.2</p>
-#         ]]>
+#         <center><img src="http://sparkle.cocoatech.com/upgrade_header.png" width="560" height="94"></center>
+#         <font face="lucida grande" size=2>
+#         <center><h2>Path Finder 7.1.1 is now available!</h2></center>
+#         <p><b>News, changes and improvements:</b></p>
+#         <ul>
+#             <li>Improved file copying between different disks/volumes.</li>
+#             <li>Fixed keyboard selection in the column view.</li>
+#             <li>Fixed scrolling of the first item in the list view in Yosemite.</li>
+#             <li>Fixed memory leak when previewing PDF documents in Yosemite.</li>
+#             <li>Fixed drawing of the Sidebar in shelf modules in Yosemite.</li>
+#             <li>Other minor fixes and performance and stability improvements.</li>
+#         </ul>
+#         </font> 
+#     ]]>
 #     </notes>
 # </item>
