05111d43268218bae334e5701cb2255f242f2f28
Jason P. Stanford <jps3@lehigh.edu>
2015-11-03 12:33:27 -0500

    Removed all references and code for beta downloads.
    It was getting far too complex to provide for the beta versions with
    these recipes.

--- Cura/CuraUrlProvider.py
+++ Cura/CuraUrlProvider.py
@@ -23,17 +23,12 @@ import platform
 from distutils.version import LooseVersion
 from autopkglib import Processor, ProcessorError
 
-# Last Checked: Monday; November 2, 2015
-# `latest.xml` appears to be for current stable releases
-# `latest.json` appears to be for current beta releases
-import json
 import xml.etree.ElementTree as ET
 
 
 __all__ = ["CuraUrlProvider"]
 
 CURA_URL_STUB_DEFAULT = 'http://software.ultimaker.com'
-CURA_RELEASE_TYPE_DEFAULT = 'stable'
 CURA_EXT = 'dmg'
 
 
@@ -43,11 +38,6 @@ class CuraUrlProvider(Processor):
     URL to download their installer file.
     """
     input_variables = {
-        "release_type": {
-            "required": False,
-            "default": CURA_RELEASE_TYPE_DEFAULT,
-            "description": "Value can be `stable` (default) or `beta`.",
-        },
         "update_url_stub": {
             "required": False,
             "default": CURA_URL_STUB_DEFAULT,
@@ -60,27 +50,6 @@ class CuraUrlProvider(Processor):
         },
     }
     __doc__ = description
-
-    def parse_json_feed_make_filepath(self, json_data):
-        """
-        Parses the JSON feed for latest (assumedly) beta version, and attempts to 
-        construct and return a valid URL path for download.
-        """
-        getdatum = lambda s: latest['cura']['Darwin'][s]
-        try:
-            latest = json.loads(json_data)
-            release_major = int(getdatum('major'))
-            release_minor = "{:02d}".format(int(getdatum('minor')))
-            release_revision = "{:02d}".format(int(getdatum('revision')))
-            filepath = "{maj}.{min}/Cura-{maj}.{min}.{rev}-{sys}.{ext}".format(
-                maj=release_major, min=release_minor, rev=release_revision,
-                sys=platform.system(), ext=CURA_EXT)
-            assert filepath is not None
-        except Exception as e:
-            raise ProcessorError(
-                "Error parsing JSON data from feed. ('{}')".format(e))
-
-        return filepath
 
     def parse_xml_feed_make_filepath(self, xml_data):
         """
@@ -97,17 +66,11 @@ class CuraUrlProvider(Processor):
         except Exception as e:
             raise ProcessorError(
                 "Error parsing XML data from feed. ('{}')".format(e))
-
         return filepath
 
     def get_latest_feed_file(self, stub_url=CURA_URL_STUB_DEFAULT):
-        """Retrieves version feed file, which differs based on `release_type`."""
-
-        if self.env["release_type"].lower() == 'beta':
-            feed_url = '{stub}/latest.{ext}'.format(stub=stub_url, ext='json')
-        else:
-            feed_url = '{stub}/latest.{ext}'.format(stub=stub_url, ext='xml')
-
+        """Retrieves version feed file."""
+        feed_url = '{stub}/latest.{ext}'.format(stub=stub_url, ext='xml')
         try:
             request = urllib2.Request(url=feed_url)
             url_handle = urllib2.urlopen(request)
@@ -115,17 +78,7 @@ class CuraUrlProvider(Processor):
         except:
             raise ProcessorError(
                 "Could not open or read update feed url: {u}".format(u=url))
-
-        mime_type = url_handle.headers.type
-
-        if mime_type == 'application/json':
-            filepath = self.parse_json_feed_make_filepath(feed_data)
-        elif mime_type == 'application/xml':
-            filepath = self.parse_xml_feed_make_filepath(feed_data)
-        else:
-            raise ProcessorError(
-                "No handler for feed Content-Type of '{t}'".format(t=mime_type))
-
+        filepath = self.parse_xml_feed_make_filepath(feed_data)
         return urllib2.urlparse.urljoin(stub_url, filepath)
 
     def main(self):
